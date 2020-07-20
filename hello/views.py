4ef5529337627da2098c983fe279d87fcf9ed214
Kimberly190 <kimberly.zito@gmail.com>
2020-07-20 11:48:21 -0400

    Added form and view to log a message

--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,9 @@ import re
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
 
 # TEST URLS
 #TODO Remove before ship
@@ -17,6 +20,18 @@ def about(request):
 
 def contact(request):
     return render(request, 'hello/contact.html')
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect('home')
+    else:
+        return render(request, 'hello/log_message.html', {'form': form})
 
 def hello_there(request, name):
     return render(
