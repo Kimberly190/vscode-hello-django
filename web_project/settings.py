3a03a8efd0888d71a7a67066e45622f4c45337ec
Kimberly Z <Kimberly190@users.noreply.github.com>
2020-07-24 23:14:32 -0400

    Removed secret key from Git-tracked settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%a96094+2+4=pv*70p3g@d7738#ppy=k(0^#3+$z0cz0y9dyhm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
